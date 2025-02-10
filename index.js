1f85700a43a654fd1cc78300eb4906f4a8ab5052
koo1004kh <koo1004kh@gmail.com>
2025-02-10 23:19:11 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+//1번 문제
+let a = 1
+let b = 2 
+tmp=b
+b=a
+a=tmp
+
+console.log(a,b)
+
+//2번
+20 + 30  <50>
+“20” + “30”   <2030>
+“Hello” + " " + 2021   <Hello2021>
+1 + 2 * 3       <7>
+(1 + 3) ** 2    <16>
+1 / 0           <불가능>
+6 % 2            <0>
+7.5 % 2         <1.5>
+5 == 5          <True>
+5 === 5         <True>
+5 == “5”        <True>
+5 === “5”       <False>
+5 != 5.0        <False>
+“true” === true <False>
+5 <= 5.0        <True>
+5 >= 5          (TRUE)
+true || true    (TRUE)
+true || false   (TRUE)
+true && true    (TRUE)
+true && false   (FALSE)
+!true           (FALSE)
+!false          (TRUE)
